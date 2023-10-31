e212ddb2bc6e2c9dcab726e2a73dda105f2dd82d
P.D.Kaumadi <kaumadi2k2@gmail.com>
2023-10-31 14:27:27 +0530

    remove unneccessery files

--- public/assets/js/customer/home.js
+++
@@ -1,4 +0,0 @@
-// JavaScript to handle the button click and show the login page
-document.getElementById("loginButton").addEventListener("click", function () {
-    document.getElementById("loginPage").style.display = "block";
-});
