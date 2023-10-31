e42974b0f2b596611b4d395dfc8cfef59a419a6e
pathu11 <pathuahinsa2001@gmail.com>
2023-10-31 12:59:57 +0530

    change AddUsedBook.php

--- app/views/customer/AddUsedBook.php
+++ app/views/customer/AddUsedBook.php
@@ -4,196 +4,164 @@
 ?>
 
     <div class="container">
-        <div class="sidebar">
-        <!-- Sidebar content goes here -->
-        <div class="profile-section">
-            <img src="./assets/img/profile.png" alt="Profile Image" class="profile-image">
-            <?php 
-            if (isset($_SESSION["customerName"])){
-                echo '<h2 class="profile-name1">'.$_SESSION["customerName"].'</h2>';
-            } else {
-                echo '<h2 class="profile-name1">Ramath Perera</h2>';
-            }
-            ?>
-        </div>
-        <br>
-        <hr>
-
-        <!-- Menu section -->
-        <div class="menu-section">
-            <ul class="menu-list">
-                <li data-page="Dashboard"><a href="./Dashboard.php">Dashboard</a></li>
-                <li data-page="Profile"><a href="./Profile.php">Profile</a></li>
-                <li data-page="Notification"><a href="./Notification.php">Notification</a></li>
-                <li data-page="Bookshelf"><a href="./Bookshelf.php">Bookshelf</a></li>
-                <li data-page="Content"><a href="./Content.php">Content</a></li>
-                <li data-page="Cart"><a href="./Cart.php">Cart</a></li>
-            </ul>
-        </div>
-        </div>
+        <?php
+            include_once 'sidebar.php';
+        ?>
 
         <div class="add-content">
-            <form action="#" class="book-add">
+            <form action="http://localhost/Group-27/app/controllers/customer/addusedbooks.inc.php" class="book-add">
 
                 <h1>Add a Used Book</h1>
                 
                 <div class="topic-book">
-                    <label class="label-topic" required>Book Name</label><br>
-                    <input type="text" class="form-topic">
+                    <label class="label-topic">Book Name</label><br>
+                    <input type="text" class="form-topic"  name="bookName"  required>
                 </div>
                 
                 <div class="topic-book author">
-                    <label class="label-topic" required>Author of Book</label><br>
-                    <input type="text" class="form-topic">
+                    <label class="label-topic">Author of Book</label><br>
+                    <input type="text" class="form-topic"  name="author" required>
                 </div>
                 
 
                 <div class="upload-pages book-cate">
                     <div class="topic-book author">
-                        <label class="label-topic" required>Book Category</label><br>
-                        <select id="category" name="category" required>
+                        <label class="label-topic">Book Category</label><br>
+                        <select id="category"  name="category" required>
                             <option value="technology">Technology</option>
                             <option value="travel">Travel</option>
                             <option value="food">Food</option>
                             <option value="lifestyle">Lifestyle</option>
                             <option value="health">Health</option>
-                            <!-- Add more categories as needed -->
                         </select>
-                        <!-- <input type="text" class="form-topic"> -->
                     </div>
                 
                     <div class="topic-book author">
-                        <label class="label-topic" required>Condition</label><br>
-                        <select id="category" name="category" required>
+                        <label class="label-topic">Condition</label><br>
+                        <select id="category"  name="bookCondition" required>
                             <option value="technology">Technology</option>
                             <option value="travel">Travel</option>
                             <option value="food">Food</option>
                             <option value="lifestyle">Lifestyle</option>
                             <option value="health">Health</option>
-                            <!-- Add more categories as needed -->
                         </select>
-                        <!-- <input type="text" class="form-topic"> -->
                     </div>
 
                     <div class="topic-book author">
-                        <label class="label-topic" required>Published Date</label><br>
-                        <input type="Date" class="form-topic">
+                        <label class="label-topic">Published Year</label><br>
+                        <input type="number" class="form-topic"  name="publishedYear" required min=1800 max=2100>
                     </div>
                 </div>
 
 
                 <div class="upload-pages book-cate">
                     <div class="topic-book author">
-                        <label class="label-topic" required>Price</label><br>
-                        <input type="text" class="form-topic">
+                        <label class="label-topic">Price</label><br>
+                        <input type="number" class="form-topic"  name="price" required>
                     </div>
     
                     <div class="topic-book author">
-                        <label class="label-topic" required>Price Type</label><br>
-                        <select id="category" name="category" required>
-                        <!-- <option value="">Select a type</option> -->
+                        <label class="label-topic">Price Type</label><br>
+                        <select id="category"  name="priceType" required>
                             <option value="technology">Technology</option>
                             <option value="travel">Travel</option>
                             <option value="food">Food</option>
                             <option value="lifestyle">Lifestyle</option>
                             <option value="health">Health</option>
-                            <!-- Add more categories as needed -->
                         </select>
                     </div>
         
                     <div class="topic-book author weight">
-                        <label class="label-topic" required>Weight (grams)</label><br>
-                        <input type="text" class="form-topic" required>
+                        <label class="label-topic">Weight (grams)</label><br>
+                        <input type="number" class="form-topic"  name="weight" required>
                         <a href="#"><button class="weight-cal">Weight Calculator</button></a>
                     </div>
                 </div>
         
                 <div class="topic-book author">
-                    <label class="label-topic" for="input1" required>ISBN Number</label><br>
-                    <input type="text" class="form-topic" id="input1">
+                    <label class="label-topic" for="input1">ISBN Number</label><br>
+                    <input type="text" class="form-topic" id="input1" name="isbnNumber">
                 </div>
         
                 <div class="topic-book author">
-                    <label class="label-topic" for="input2" required>ISSN Number</label><br>
-                    <input type="text" class="form-topic" id="input2">
+                    <label class="label-topic" for="input2">ISSN Number</label><br>
+                    <input type="text" class="form-topic" id="input2" name="issnNumber">
                 </div>
         
                 <div class="topic-book author">
-                    <label class="label-topic" for="input3" required>ISMN Number</label><br>
-                    <input type="text" class="form-topic" id="input3">
+                    <label class="label-topic" for="input3">ISMN Number</label><br>
+                    <input type="text" class="form-topic" id="input3" name="issmNumber">
                 </div>
         
                 <div class="disc-book">
                     <label class="label-topic">Description</label><br>
-                    <textarea id="description" name="description" rows="12" class="form-topic" required></textarea>
+                    <textarea id="description" rows="12" class="form-topic"  name="description" required></textarea>
                 </div>
         
                 <div class="upload-pages">
                     <div class="img-cont">
                         <label class="label-topic">Upload Front Page</label><br>
-                        <input type="file" id="picture" name="picture" accept="image/*" required>
+                        <input type="file" id="picture" accept="image/*"  name="imgFront" required>
                     </div>
         
                     <div class="img-cont">
                         <label class="label-topic">Upload Back Page</label><br>
-                        <input type="file" id="picture" name="picture" accept="image/*" required>
+                        <input type="file" id="picture" accept="image/*"  name="imgBack" required>
                     </div>
         
                     <div class="img-cont">
                         <label class="label-topic">Upload a Inside Page</label><br>
-                        <input type="file" id="picture" name="picture" accept="image/*" required>
+                        <input type="file" id="picture" accept="image/*"  name="imgInside" required>
                     </div>
                 </div>
                 <hr>
                 <div class="topic-book">
-                    <label class="label-topic" required>Account Holder's Name</label><br>
-                    <input type="text" class="form-topic">
+                    <label class="label-topic">Account Holder's Name</label><br>
+                    <input type="text" class="form-topic"  name="accName" required>
                 </div>
                 
                 <div class="topic-book author">
-                    <label class="label-topic" required>Account Number</label><br>
-                    <input type="number" class="form-topic">
+                    <label class="label-topic">Account Number</label><br>
+                    <input type="number" class="form-topic"  name="accNumber" required>
                 </div>
 
                 <div class="upload-pages">
                     <div class="topic-book author">
-                        <label class="label-topic" required>Bank Name</label><br>
-                        <input type="text" class="form-topic">
+                        <label class="label-topic">Bank Name</label><br>
+                        <input type="text" class="form-topic"  name="bankName" required>
                     </div>
 
                     <div class="topic-book author">
-                        <label class="label-topic" required>Branch Name</label><br>
-                        <input type="text" class="form-topic">
+                        <label class="label-topic">Branch Name</label><br>
+                        <input type="text" class="form-topic"  name="branchName" required>
                     </div>
                 </div>
                 <hr>
 
                 <div class="upload-pages book-cate">
                     <div class="topic-book author">
-                        <label class="label-topic" required>Town</label><br>
-                        <input type="text" class="form-topic">
+                        <label class="label-topic">Town</label><br>
+                        <input type="text" class="form-topic"  name="town" required>
                     </div>
     
                     <div class="topic-book author">
-                        <label class="label-topic" required>District</label><br>
-                        <select id="category" name="category" required>
-                        <!-- <option value="">Select a type</option> -->
+                        <label class="label-topic">District</label><br>
+                        <select id="category"  name="district" required>
                             <option value="technology">Technology</option>
                             <option value="travel">Travel</option>
                             <option value="food">Food</option>
                             <option value="lifestyle">Lifestyle</option>
                             <option value="health">Health</option>
-                            <!-- Add more categories as needed -->
                         </select>
                     </div>
 
                     <div class="topic-book author">
-                        <label class="label-topic" required>Postal Code</label><br>
-                        <input type="number" class="form-topic">
+                        <label class="label-topic">Postal Code</label><br>
+                        <input type="number" class="form-topic"  name="postalCode" required>
                     </div>
                 </div>
 
-                <input type="submit" value="Submit">
+                <input type="submit" value="Submit" name="submitused">
             </form>
         </div>
     </div>
