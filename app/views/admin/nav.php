8c320bee0ce609783481ed41e9651a98c0fe89bc
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2023-11-17 12:57:49 +0530

    changed
    changed

--- app/views/admin/nav.php
+++ app/views/admin/nav.php
@@ -66,7 +66,7 @@
               </a>
             </li>
             <li class="list">
-              <a href="#" class="nav-link">
+              <a href="<?php echo URLROOT; ?>/landing/logout" class="nav-link">
                 <i class="bx bxs-log-out icon"></i>
                 <span class="link">Logout</span>
               </a>
--- app/views/landing/header.php
+++ app/views/landing/header.php
@@ -1,51 +1,4 @@
-<<<<<<< HEAD
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/LandingPage.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/head.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/footer.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/prof.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/AddBookConte.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/BuyBook.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/Sevices.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/Aboutus.css">
-    <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/customer/BookDetails.css">
-
-    <!-- <script src="./assets/js/prof.js"></script> -->
-    <script src="<?php echo URLROOT; ?>/assets/js/customer/service.js"></script>
-    <script src="<?php echo URLROOT; ?>/assets/js/customer/Add.js"></script>
-    <script src="<?php echo URLROOT; ?>/assets/js/customer/dropcategory.js"></script>
-    <script src="<?php echo URLROOT; ?>/assets/js/customer/home.js"></script>
-
-    <title><?php echo $title; ?></title>
-</head>
-<body>
-    <header>
-    <div class="head">
-        <img src="<?php echo URLROOT; ?>/assets/images/customer/logo.png" alt="logo" class="logo">
-        <div class="navig">
-            <nav class="navigation">
-                <?php 
-                // if (isset($_SESSION["customer_name"])){
-                //     echo '<a href="http://localhost/Group-27/app/views/customer/Home.php">Home</a>';
-                // } else {
-                //     echo '<a href="./index.php">Home</a>';
-                // }
-                ?>
-                <a href="<?php echo URLROOT; ?>/landing/logout">Logout</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
-            </nav>
-           
-        </div>
-        </div>
-    </header>
-=======
 
 <!DOCTYPE html>
 <html lang="en">
@@ -100,5 +53,3 @@
         </div>
         </div>
     </header>
->>>>>>> d8c71934c53defc273670933be317035e003477f
-    
