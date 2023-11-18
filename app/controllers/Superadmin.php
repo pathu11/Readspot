d3f8031c604b2f9adeecba044e939eeeff24b67e
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2023-11-18 07:48:14 +0530

    Add superadmin files

---
+++ app/controllers/Superadmin.php
@@ -0,0 +1,13 @@
+
+<?php
+class Superadmin extends Controller{
+    public function  __construct(){
+       
+    }
+    public function index(){
+        
+        $this->view('superadmin/index');
+    }
+    
+
+}
