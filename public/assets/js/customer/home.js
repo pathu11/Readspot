75fa81b540057bad8b38fa8c369a66852783d71a
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2023-11-18 14:16:40 +0530

    add some js files

---
+++ public/assets/js/customer/home.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the button click and show the login page
+document.getElementById("loginButton").addEventListener("click", function () {
+    document.getElementById("loginPage").style.display = "block";
+});
