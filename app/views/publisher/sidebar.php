5d001b6e3bb1c9881844083b54b9c25408fc5a76
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2024-01-02 14:31:28 +0530

    change tables

--- app/views/publisher/sidebar.php
+++ app/views/publisher/sidebar.php
@@ -54,6 +54,7 @@
                 <span class="link">Events</span>
               </a>
             </li>
+            
             <li class="list">
               <a href="<?php echo URLROOT; ?>/publisher/setting" class="nav-link">
                 <i class="bx bxs-book-heart icon"></i>
@@ -67,6 +68,7 @@
                 <span class="link">Notifications</span>
               </a>
             </li>
+            <br><br><br><br>
             <li class="list">
               <a href="<?php echo URLROOT; ?>/publisher/logout" class="nav-link">
                 <i class="bx bxs-log-out icon"></i>
