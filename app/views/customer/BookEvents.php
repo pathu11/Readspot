4db04458b0751053a9404e584ac384fe6f91bc3c
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-06 21:07:04 +0530

    Change BookEvents.php, main.css and Add BookEvents.css filess

--- app/views/customer/BookEvents.php
+++ app/views/customer/BookEvents.php
@@ -4,21 +4,21 @@
 ?>
 
     <div class="main-cont">
-        <div class="sub-cont1">
-            <div class="new-books">
+        <div class="sub-cont-E1">
+            <div class="Event-books">
                 <h1>BOOK EVENTS</h2>
             </div>
         </div>
-        <div class="sub-cont2">
-            <div class="B0">
-                <img src="<?php echo URLROOT; ?>/assets/images/customer/event1.jpg" alt="Book1" class="event"> <!--path changed-->
+        <div class="sub-cont-E2">
+            <div class="B0-E">
+                <img src="<?php echo URLROOT; ?>/assets/images/customer/event1.jpg" alt="Book1" class="event-E"> <!--path changed-->
                 
-                <a href="<?php echo URLROOT; ?>/customer/viewevents"><button class="dts-btn">View Event</button></a> <!--path changed-->
+                <a href="<?php echo URLROOT; ?>/customer/viewevents"><button class="dts-btn-E">View Event</button></a> <!--path changed-->
             </div>
-            <div class="B0">
-                <img src="<?php echo URLROOT; ?>/assets/images/customer/event2.jpg" alt="Book2" class="event"> <!--path changed-->
+            <div class="B0-E">
+                <img src="<?php echo URLROOT; ?>/assets/images/customer/event2.jpg" alt="Book2" class="event-E"> <!--path changed-->
                 
-                <a href="<?php echo URLROOT; ?>/customer/viewevents"><button class="dts-btn">View Event</button></a> <!--path changed-->
+                <a href="<?php echo URLROOT; ?>/customer/viewevents"><button class="dts-btn-E">View Event</button></a> <!--path changed-->
             </div>
             <!-- <div class="B0">
                 <img src="http://localhost/Group-27/public/assets/images/customer/book.jpg" alt="Book3" class="event">
