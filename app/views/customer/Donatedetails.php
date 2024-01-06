bb3ea66a0f1e8cedc9f39531e4223a9a8bef3232
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-07 01:20:53 +0530

    Change Donatedetails.php and viewevents.php file->css part

--- app/views/customer/Donatedetails.php
+++ app/views/customer/Donatedetails.php
@@ -3,26 +3,28 @@
     require APPROOT . '/views/customer/header.php'; //path changed
 ?>
 
-    <div class="donate-event-detail">
-        <div class="event-img-div">
-            <img src="<?php echo URLROOT; ?>/assets/images/customer/donate1.jpg" alt="Book3" class="main-img"> <!--path changed-->
+    <div class="donate-detail">
+        <div class="donate-main">
+            <div class="donate-img-div">
+                <img src="<?php echo URLROOT; ?>/assets/images/customer/donate1.jpg" alt="Book3" class="donate-img"> <!--path changed-->
+            </div>
+            <div class="req-books-details-D">
+                <h2>Required Books Types</h2><br>
+                <ul>
+                    <li>Science Fiction</li>
+                    <li>Fantasy</li>
+                    <li>Horror</li>
+                    <li>Classics</li>
+                    <li>Historical Fiction</li>
+                    <!-- <li>Lorem ipsum dolor sit amet.</li> -->
+                </ul>
+            </div>
         </div>
-        <div class="event-des-div">
+        <div class="donate-des-div">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque commodi hic saepe delectus beatae sit sunt a deleniti nobis minima explicabo, deserunt omnis. Sequi dicta reiciendis, doloremque ut enim voluptatem sint, corporis, laboriosam error fugit nostrum possimus necessitatibus voluptatibus alias consequatur. Pariatur placeat quas sapiente sit expedita adipisci voluptatem ut quia, porro natus ea dolore nam veritatis dignissimos vero maiores velit. Doloribus facilis iure enim dolorem magnam fuga quis debitis delectus repellendus nostrum? Fugit exercitationem doloremque rerum earum quia sint incidunt nisi maiores recusandae deserunt aperiam, eveniet distinctio assumenda voluptate vero soluta, similique id nihil iste voluptas voluptates omnis voluptatibus. Maxime consequatur nihil animi fuga facere nulla! Itaque dicta suscipit quia repudiandae eius nostrum necessitatibus facere quidem expedita sint, consequatur vel atque distinctio error dolore nam porro fugiat laudantium! Error, voluptatibus commodi? Dolorum, iure quasi fugiat earum voluptatibus voluptas ipsum consequuntur? Quae, temporibus ut voluptate libero itaque omnis debitis. Quia suscipit cumque sit vel culpa repudiandae tempore natus hic distinctio! Perspiciatis mollitia excepturi itaque quod suscipit, quis culpa consequatur ratione, ullam consectetur odio fugiat accusantium exercitationem aliquid amet consequuntur nulla aut ipsa doloremque hic inventore molestias debitis. Velit alias omnis recusandae ullam. Ex, sit voluptate! Veritatis, sequi, nisi ipsam soluta consectetur nesciunt sapiente delectus incidunt aperiam repudiandae suscipit dolore nemo nobis necessitatibus iste. Odio provident, perspiciatis commodi fugiat iure atque maiores corporis obcaecati sequi pariatur, alias impedit temporibus assumenda. Amet rerum accusantium necessitatibus enim voluptatem explicabo ipsam voluptates vitae optio modi saepe nostrum harum deserunt porro blanditiis voluptatum, officia quia!</P>
         </div>
-        <div class="req-book-details">
-            <h2>Required Books Types</h2><br>
-            <ul>
-                <li>Science Fiction</li>
-                <li>Fantasy</li>
-                <li>Horror</li>
-                <li>Classics</li>
-                <li>Historical Fiction</li>
-                <!-- <li>Lorem ipsum dolor sit amet.</li> -->
-            </ul>
-        </div>
-        <div class="event-btn-div">
-            <a href="<?php echo URLROOT; ?>/customer/Donateform"><button class="chat-btn">Donate</button></a> <!--path changed-->
+        <div class="donate-btn-div">
+            <a href="<?php echo URLROOT; ?>/customer/Donateform"><button class="donate-chat-btn">Donate</button></a> <!--path changed-->
         </div>
     </div>
 
