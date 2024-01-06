77665710683407cfe84bcbbff765f4350986bff2
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-07 00:09:58 +0530

    Change the footer

--- app/views/customer/footer.php
+++ app/views/customer/footer.php
@@ -7,25 +7,26 @@
             <img src="<?php echo URLROOT; ?>/assets/images/customer/twitter.png" alt="Service 3">
             <img src="<?php echo URLROOT; ?>/assets/images/customer/instagram.png" alt="Service 3">
             <img src="<?php echo URLROOT; ?>/assets/images/customer/facebook.png" alt="Service 3">
+            <img src="<?php echo URLROOT; ?>/assets/images/customer/linkedin.png" alt="Service 3">
         </div>
         <div class="bookcata">
             <h4>Book category</h4>
             <ul>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Action and Adventure</a></li>
+                <li><a href="#">Classics</a></li>
+                <li><a href="#">Fantasy</a></li>
+                <li><a href="#">Horror</a></li>
+                <li><a href="#">Romance</a></li>
+                <li><a href="#">Science Fiction (Sci-Fi)</a></li>
             </ul>
         </div>
         <div class="quicklink">
-            <h4>Site Map</h4>
+            <h4>Quick Link</h4>
             <ul>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">FAQ</a></li>
                 <li><a href="#">Privacy Policy</a></li>
                 <li><a href="#">Terms of Service</a></li>
             </ul>
