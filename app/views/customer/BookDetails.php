032af576eeddd65da9e21fef181c858193926cf2
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-08 14:12:23 +0530

    Change view event page css -> Add 5 pictures to it.

--- app/views/customer/BookDetails.php
+++ app/views/customer/BookDetails.php
@@ -60,62 +60,66 @@
                     <p>4.1 average based on 254 reviews.</p>
                     <hr style="border:3px solid #f1f1f1">
 
-                    <div class="row">
-                    <div class="side">
-                        <div>5 star</div>
-                    </div>
-                    <div class="middle">
-                        <div class="bar-container">
-                        <div class="bar-5"></div>
+                    <div class="row-rating">
+                        <div class="side">
+                            <div>5 star</div>
                         </div>
-                    </div>
-                    <div class="side right">
-                        <div>150</div>
-                    </div>
-                    <div class="side">
-                        <div>4 star</div>
-                    </div>
-                    <div class="middle">
-                        <div class="bar-container">
-                        <div class="bar-4"></div>
+                        <div class="middle">
+                            <div class="bar-container">
+                                <div class="bar-5"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="side right">
-                        <div>63</div>
-                    </div>
-                    <div class="side">
-                        <div>3 star</div>
-                    </div>
-                    <div class="middle">
-                        <div class="bar-container">
-                        <div class="bar-3"></div>
+                        <div class="side right">
+                            <div>150</div>
                         </div>
-                    </div>
-                    <div class="side right">
-                        <div>15</div>
-                    </div>
-                    <div class="side">
-                        <div>2 star</div>
-                    </div>
-                    <div class="middle">
-                        <div class="bar-container">
-                        <div class="bar-2"></div>
+                        
+                        <div class="side">
+                            <div>4 star</div>
                         </div>
-                    </div>
-                    <div class="side right">
-                        <div>6</div>
-                    </div>
-                    <div class="side">
-                        <div>1 star</div>
-                    </div>
-                    <div class="middle">
-                        <div class="bar-container">
-                        <div class="bar-1"></div>
+                        <div class="middle">
+                            <div class="bar-container">
+                                <div class="bar-4"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="side right">
-                        <div>20</div>
-                    </div>
+                        <div class="side right">
+                            <div>63</div>
+                        </div>
+                        
+                        <div class="side">
+                            <div>3 star</div>
+                        </div>
+                        <div class="middle">
+                            <div class="bar-container">
+                                <div class="bar-3"></div>
+                            </div>
+                        </div>
+                        <div class="side right">
+                            <div>15</div>
+                        </div>
+                        
+                        <div class="side">
+                            <div>2 star</div>
+                        </div>
+                        <div class="middle">
+                            <div class="bar-container">
+                                <div class="bar-2"></div>
+                            </div>
+                        </div>
+                        <div class="side right">
+                            <div>6</div>
+                        </div>
+                        
+                        <div class="side">
+                            <div>1 star</div>
+                        </div>
+                        <div class="middle">
+                            <div class="bar-container">
+                                <div class="bar-1"></div>
+                            </div>
+                        </div>
+                        <div class="side right">
+                            <div>20</div>
+                        </div>
                     </div>
                 </div>
                 <div class="give-rate">
@@ -186,6 +190,7 @@
                     <h5>13 people found this helpful</h5>
                 </div>
             </div>
+        </div>
 
         <div class="sub8">
             <a href="#"><button class="chat-btn">Chat</button></a>
