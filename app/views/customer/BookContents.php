e0ddac3979674ff40b2a2624de9175bd320ff5f9
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-08 15:27:14 +0530

    Change viewcontent page

--- app/views/customer/BookContents.php
+++ app/views/customer/BookContents.php
@@ -23,46 +23,67 @@
         </div>
         <div class="sub-cont-C2">
             <div class="content0-C">
-                <img src="<?php echo URLROOT; ?>/assets/images/customer/cont1.jpeg" alt="Book3" class="content-img-C"> <!--path changed-->
-                <h1>Explore the Stars</h1><br>
-                <p>Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Accusantium, numquam! 
-                    Voluptas voluptates quas modi debitis molestias
-                    cumque voluptatum fugit laudantium voluptatibus 
-                    repellat facere optio cupiditate, vel, aperiam 
-                    aliquam consequatur perspiciatis iusto enim quaerat. 
-                    Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
-                    sit ullam sint dolores iure natus commodi veniam aliquid odit.
-                </p><br>
-                <a href="#"><button class="vw-btn-C">View Details</button></a>
+                <div class="content1-C">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/cont1.jpeg" alt="Book3" class="content-img-C"> <!--path changed-->
+                    <div class="content2-C">
+                        <h1>Explore the Stars</h1><br>
+                        <p>Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Accusantium, numquam! 
+                            Voluptas voluptates quas modi debitis molestias
+                            cumque voluptatum fugit laudantium voluptatibus 
+                            repellat facere optio cupiditate, vel, aperiam 
+                            aliquam consequatur perspiciatis iusto enim quaerat. 
+                            Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
+                            sit ullam sint dolores iure natus commodi veniam aliquid odit.
+                        </p>
+                    </div>
+                </div>
+                <div class="view-fav">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/favorit.png" alt="Favorit">
+                    <a href="<?php echo URLROOT; ?>/customer/viewcontent"><button class="vw-btn-C">View Details</button></a>
+                </div>
             </div>
             <div class="content0-C">
-                <img src="<?php echo URLROOT; ?>/assets/images/customer/cont2.jpeg" alt="Book3" class="content-img-C"> <!--path changed-->
-                <h1>Business Law</h1><br>
-                <p>Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Accusantium, numquam! 
-                    Voluptas voluptates quas modi debitis molestias
-                    cumque voluptatum fugit laudantium voluptatibus 
-                    repellat facere optio cupiditate, vel, aperiam 
-                    aliquam consequatur perspiciatis iusto enim quaerat. 
-                    Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
-                    sit ullam sint dolores iure natus commodi veniam aliquid odit.
-                </p><br>
-                <a href="#"><button class="vw-btn-C">View Details</button></a>
+                <div class="content1-C">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/cont2.jpeg" alt="Book3" class="content-img-C"> <!--path changed-->
+                    <div class="content2-C">
+                        <h1>Business Law</h1><br>
+                        <p>Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Accusantium, numquam! 
+                            Voluptas voluptates quas modi debitis molestias
+                            cumque voluptatum fugit laudantium voluptatibus 
+                            repellat facere optio cupiditate, vel, aperiam 
+                            aliquam consequatur perspiciatis iusto enim quaerat. 
+                            Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
+                            sit ullam sint dolores iure natus commodi veniam aliquid odit.
+                        </p>
+                    </div>
+                </div>
+                <div class="view-fav">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/favorit.png" alt="Favorit">
+                    <a href="<?php echo URLROOT; ?>/customer/viewcontent"><button class="vw-btn-C">View Details</button></a>
+                </div>
             </div>
             <div class="content0-C">
-                <img src="<?php echo URLROOT; ?>/assets/images/customer/cont3.jpg" alt="Book3" class="content-img-C"> <!--path changed-->
-                <h1>New Educators</h1><br>
-                <p>Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Accusantium, numquam! 
-                    Voluptas voluptates quas modi debitis molestias
-                    cumque voluptatum fugit laudantium voluptatibus 
-                    repellat facere optio cupiditate, vel, aperiam 
-                    aliquam consequatur perspiciatis iusto enim quaerat. 
-                    Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
-                    sit ullam sint dolores iure natus commodi veniam aliquid odit.
-                </p><br>
-                <a href="#"><button class="vw-btn-C">View Content</button></a>
+                <div class="content1-C">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/cont3.jpg" alt="Book3" class="content-img-C"> <!--path changed-->
+                    <div class="content2-C">
+                        <h1>New Educators</h1><br>
+                        <p>Lorem ipsum dolor sit amet consectetur, 
+                            adipisicing elit. Accusantium, numquam! 
+                            Voluptas voluptates quas modi debitis molestias
+                            cumque voluptatum fugit laudantium voluptatibus 
+                            repellat facere optio cupiditate, vel, aperiam 
+                            aliquam consequatur perspiciatis iusto enim quaerat. 
+                            Laboriosam, debitis cum. Pariatur consequatur rem tenetur, 
+                            sit ullam sint dolores iure natus commodi veniam aliquid odit.
+                        </p>
+                    </div>
+                </div>
+                <div class="view-fav">
+                    <img src="<?php echo URLROOT; ?>/assets/images/customer/favorit.png" alt="Favorit">
+                    <a href="<?php echo URLROOT; ?>/customer/viewcontent"><button class="vw-btn-C">View Details</button></a>
+                </div>
             </div>
         </div>
     </div>
