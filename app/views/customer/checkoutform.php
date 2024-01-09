7a97002a4c9f7353d61ab03e53d6d226c9571c47
M.K.P.Ahinsa <pathuahinsa2001@gmail.com>
2024-01-09 09:58:43 +0530

    add new file

--- app/views/customer/checkoutform.php
+++ app/views/customer/checkoutform.php
@@ -4,145 +4,65 @@
 ?>
 
     <div class="checkout-main">
-        <div class="check-form">
-        <!-- <form action="/action_page.php" class="check-form"> -->
+        <form action="/action_page.php" class="check-form">
             <div class="check-form-main">
                 <div class="billing-address">
-                <label>
-                    <input type="radio" name="paymentType" value="cardPayment">
-                        Card Payment
-                </label>
-                <label>
-                    <input type="radio" name="paymentType" value="OnlineDeposit">
-                        Online Deposit
-                </label>
-                <label>
-                    <input type="radio" name="paymentType" value="COD">
-                        Cash On Delivery
-                </label>
-                   
+                    <h3>Billing Address</h3>
+                    
+                    <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+                    <input type="text" id="fname" name="firstname" placeholder="Ramath Perera">
+                    
+                    <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+                    <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
+
+                    <label for="phone"><i class="fa fa-phone"></i> Telephone</label>
+                    <input type="text" id="email" name="email" placeholder="07123456789">
+
+                    <div class="row">
+                        <div class="col-50">
+                            <label for="district">District</label>
+                            <input type="text" id="district" name="district" placeholder="Kaluthara">
+                        </div>
+                        <div class="col-50">
+                            <label for="city">City</label>
+                            <input type="text" id="city" name="city" placeholder="Panadura">
+                        </div>
+                        <div class="col-50">
+                            <label for="zip">Zip</label>
+                            <input type="text" id="zip" name="zip" placeholder="12500">
+                        </div>
+                    </div>
                 </div>
                 <div class="payment">
                     <h3>Payment</h3>
-                    <div class="cardPayment-form">
-                    
-                        <button onClick="paymentGateway();">Pay here</button>
-                        <script type="text/javascript" src="https://www.payhere.lk/lib/payhere.js"></script>
-                  
+                    <label for="fname">Accepted Cards</label>
+                    <div class="icon-container">
+                        <i class="fa fa-cc-visa" style="color:navy;"></i>
+                        <i class="fa fa-cc-mastercard" style="color:red;"></i>
                     </div>
-                    <div class="onlineBanking-form">
-                    <p>Make your payment directly into our bank account. Please use your Order ID as the payment reference. Your order will not be shipped until the funds have cleared in our account.
-
-                        Bank Name: Hatton National Bank - Hulftsdorp Branch
-                        Acc. Name: M.D. Gunasena & Co. (Pvt.) Ltd.
-                        Acc. No: 063010004901
-                        BIC/Swift: HBLILKLX</p>
-                        <span>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our Privacy Policy.</span>
-                        <label>submit your bank recipt after the payment</label>
-                        <input type="file" name="recipt">
-                        <button type="submit" >place Order</button>
-                </div>
-                <div class="COD-form">
-                    <p>Pay with cash upon delivery. (We will confirm the order by a phone call before accepting so make sure to enter a valid mobile number.)
-                    </p>
-                    <button type="submit" >place Order</button>
-                </div>
-
-
+                    
+                    <label for="cname">Name on Card</label>
+                    <input type="text" id="cname" name="cardname" placeholder="Ramath Perera">
+                    
+                    <label for="ccnum">Credit card number</label>
+                    <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+                    
+                    <div class="row">
+                        <div class="col-50">
+                            <label for="expire">Expire</label>
+                            <input type="text" id="expire" name="expire" placeholder="MM/YY">
+                        </div>
+                        <div class="col-50">
+                            <label for="cvv">CVV</label>
+                            <input type="text" id="cvv" name="cvv" placeholder="352">
+                        </div>
+                    </div>
                 </div>
             </div>
             <input type="submit" value="Pay 1700.00" class="btn-checkout">
-</div>
-        <!-- </form> -->
+        </form>
     </div>
     <?php
         require APPROOT . '/views/customer/footer.php'; //path changed
     ?>
     
-<script>
-    function paymentGateway() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                alert(xhttp.responseText);
-
-                var obj = JSON.parse(xhttp.responseText);
-
-                // PayHere initialization
-                payhere.onCompleted = function onCompleted(orderId) {
-                    console.log("Payment completed. OrderID:" + obj["order_id"]);
-                };
-
-                payhere.onDismissed = function onDismissed() {
-                    console.log("Payment dismissed");
-                };
-
-                payhere.onError = function onError(error) {
-                    console.log("Error:" + error);
-                };
-
-                var payment = {
-                    "sandbox": true,
-                    "merchant_id": "1225428",
-                    "return_url": "http://localhost/Readspot/customer/checkoutform",
-                    "cancel_url": "http://localhost/Readspot/customer/checkoutform/",
-                    "notify_url": "http://sample.com/notify",
-                    "order_id": obj["order_id"],
-                    "items": "Door bell wireles",
-                    "amount": obj["amount"],
-                    "currency": obj["currency"],
-                    "hash": obj["hash"],
-                    "first_name": obj["first_name"],
-                    "last_name": obj["last_name"],
-                    "email": obj["email"],
-                    "phone": obj["phone"],
-                    "address": obj["address"],
-                    "city": obj["city"],
-
-                    "country": "Sri Lanka",
-                    "delivery_address": "No. 46, Galle road, Kalutara South",
-                    "delivery_city": "Kalutara",
-                    "delivery_country": "Sri Lanka",
-                    "custom_1": "",
-                    "custom_2": ""
-                };
-
-                // Start PayHere payment
-                payhere.startPayment(payment);
-            }
-        };
-
-        // AJAX request to retrieve payment details
-        xhttp.open("GET", "<?php echo URLROOT; ?>/customer/checkoutform", true);
-        xhttp.send();
-    }
-    
-    document.addEventListener('DOMContentLoaded', function () {
-        var cardPaymentForm = document.querySelector('.cardPayment-form');
-        var onlineBankingForm = document.querySelector('.onlineBanking-form');
-        var codForm = document.querySelector('.COD-form');
-
-        document.querySelectorAll('input[name="paymentType"]').forEach(function (radio) {
-            radio.addEventListener('change', function () {
-                if (this.value === 'cardPayment') {
-                    cardPaymentForm.style.display = 'block';
-                    onlineBankingForm.style.display = 'none';
-                    codForm.style.display = 'none';
-                } else if (this.value === 'OnlineDeposit') {
-                    cardPaymentForm.style.display = 'none';
-                    onlineBankingForm.style.display = 'block';
-                    codForm.style.display = 'none';
-                } else if (this.value === 'COD') {
-                    cardPaymentForm.style.display = 'none';
-                    onlineBankingForm.style.display = 'none';
-                    codForm.style.display = 'block';
-                } else {
-                    cardPaymentForm.style.display = 'none';
-                    onlineBankingForm.style.display = 'none';
-                    codForm.style.display = 'none';
-                }
-            });
-        });
-    });
-
-</script>
