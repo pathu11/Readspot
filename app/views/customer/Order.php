4c47c90d9966181b966e344f9ae57d905c44f9ea
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-16 01:56:17 +0530

    Change Oder.php file and create orderdetails.php file

--- app/views/customer/Order.php
+++ app/views/customer/Order.php
@@ -6,75 +6,75 @@
         require APPROOT . '/views/customer/sidebar.php'; //path changed
     ?>
     <div class="container">
-        <div class="my-content">
-            <div class="content-topic">
-                <h2>My Contents</h2>
+        <div class="my-orders">
+            <div class="order-topic">
+                <h2>My Orders</h2>
             </div>
-            <div class="mycontent">
-            <form action="#.php" class="content-search">
+            <div class="myorder">
+            <form action="#.php" class="order-search">
                 <input type="text" placeholder="Search.." name="search">
                 <button type="submit"><img src="<?php echo URLROOT; ?>/assets/images/customer/search.png"></button> <!--path changed-->
             </form>
+            <?php
+                require APPROOT . '/views/customer/oderdetails.php';
+            ?>
             <br>
             <br>
             <table border="1">
                 <tr>
-                    <th>Contents</th>
-                    <th >Added-Date</th>
-                    <th>VIew/Delete </th>
+                    <th>Reference No.</th>
+                    <th >Delivery Status</th>
+                    <th>VIew Details</th>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Pending</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Picked up</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Picked up</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Delivered</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Picked up</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Picked up</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Delivered</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Pending</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
                 <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
+                    <td>123456789</td>
+                    <td>Delivered</td>
+                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
                 </tr>
             </table>
+            <!-- <br>
+            <br> -->
             </div>
-            <div class="c-vw">
-                <a href="<?php echo URLROOT; ?>/customer/AddCont"><button class="c-vw-btn">Add Content</button></a> <!--path changed-->
-            </div>
-            <br>
-            <br>
         </div>
         <?php
             require APPROOT . '/views/customer/footer.php'; //path changed
