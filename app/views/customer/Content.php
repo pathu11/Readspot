663b3ea377ae6825da39b61b3bcbc7d36e4a1474
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-01-27 16:58:05 +0530

    Change content table

--- app/views/customer/Content.php
+++ app/views/customer/Content.php
@@ -11,65 +11,206 @@
                 <h2>My Contents</h2>
             </div>
             <div class="mycontent">
-            <form action="#.php" class="content-search">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><img src="<?php echo URLROOT; ?>/assets/images/customer/search.png"></button> <!--path changed-->
-            </form>
+            <div class="content-search" id="searchForm" onsubmit="handleSearch()">
+                <input type="text" placeholder="Search.." name="search" id="searchInput">
+            </div>
             <br>
             <br>
-            <table border="1">
-                <tr>
-                    <th>Contents</th>
-                    <th >Added-Date</th>
-                    <th>VIew/Delete </th>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
-                <tr>
-                    <td>Lorem ipsum dolor sit amet</td>
-                    <td>03/09/2023</td>
-                    <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td>
-                </tr>
+            <table border="1" id="eventTable">
+                <thead>
+                    <tr>
+                        <th>Content Name</th>
+                        <th >Added-Date</th>
+                        <th>VIew/Delete </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>01/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet2</td>
+                        <td>02/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>03/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>04/09/2023</td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1Lorem ipsum dolor sit amet</td>
+                        <td>05/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>06/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>07/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>08/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>09/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>10/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet</td>
+                        <td>11/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet1</td>
+                        <td>12/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                    <tr>
+                        <td>Lorem ipsum dolor sit amet5</td>
+                        <td>13/09/2023</td>
+                        <td class="action-buttons">
+                        <button class="view-button" onclick="viewEvent(1)">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        <button class="delete-button" onclick="deleteEvent(1)">
+                            <i class="fas fa-trash"></i>
+                        </button>
+                        </td>
+                        <!-- <td><div class="c-vd"><a href="#" class="c-view">View</a><a href="#" class="c-delete">Delete</a></div></td> -->
+                    </tr>
+                </tbody>
             </table>
             </div>
+            <ul class="pagination" id="pagination">
+            <li id="prevButton">«</li>
+            <li class="current">1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>10</li>
+            <li id="nextButton">»</li>
+        </ul>
             <div class="c-vw">
                 <a href="<?php echo URLROOT; ?>/customer/AddCont"><button class="c-vw-btn">Add Content</button></a> <!--path changed-->
             </div>
@@ -80,5 +221,6 @@
             require APPROOT . '/views/customer/footer.php'; //path changed
         ?>
     </div>
+
 
 
---
+++ public/assets/js/customer/tables.js
@@ -0,0 +1,106 @@
+let curtPage = 1;
+let rowsPerPage = 5;
+let maxPaginationNumbers = 3;
+let rows = [];
+let searchTerm = '';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Store the rows in a separate array
+    rows = Array.from(document.getElementById("eventTable").getElementsByTagName("tbody")[0].getElementsByTagName("tr"));
+
+    setupPagination(rows);
+
+    // Add event listener for the search input
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', handleSearch);
+});
+
+function setupPagination(rows) {
+    let totalRows = rows.length;
+    let totalPages = Math.ceil(totalRows / rowsPerPage);
+    let paginationUl = document.getElementById('pagination');
+    paginationUl.innerHTML = '';
+
+    // Previous button
+    paginationUl.innerHTML += '<li onclick="changePage(-1)">«</li>';
+
+    // Display up to maxPaginationNumbers pagination numbers
+    for (let i = 1; i <= Math.min(totalPages, maxPaginationNumbers); i++) {
+        paginationUl.innerHTML += `<li onclick="changePage(${i})">${i}</li>`;
+    }
+
+    // Next button
+    paginationUl.innerHTML += '<li onclick="changePage(-2)">»</li>';
+
+    displayRows(rows, rowsPerPage, curtPage);
+}
+
+function displayRows(rows, rowsPerPage, page) {
+    let start = (page - 1) * rowsPerPage;
+    let end = start + rowsPerPage;
+
+    for (let i = 0; i < rows.length; i++) {
+        const contentName = rows[i].cells[0].innerText.toLowerCase(); // Assuming content name is in the first cell
+
+        if (i >= start && i < end && contentName.includes(searchTerm)) {
+            rows[i].style.display = '';
+        } else {
+            rows[i].style.display = 'none';
+        }
+    }
+
+    updatePaginationUI(page);
+}
+
+function updatePaginationUI(curtPage) {
+    let pagination = document.getElementById('pagination');
+    let totalPages = Math.ceil(rows.length / rowsPerPage);
+
+    // Display exactly maxPaginationNumbers pagination numbers
+    let startPage = Math.max(1, Math.min(curtPage - Math.floor(maxPaginationNumbers / 2), totalPages - maxPaginationNumbers + 1));
+    let endPage = Math.min(startPage + maxPaginationNumbers - 1, totalPages);
+
+    pagination.innerHTML = '';
+
+    // Previous button
+    pagination.innerHTML += '<li onclick="changePage(-1)">«</li>';
+
+    // Display exactly maxPaginationNumbers pagination numbers
+    for (let i = startPage; i <= endPage; i++) {
+        pagination.innerHTML += `<li onclick="changePage(${i})">${i}</li>`;
+    }
+
+    // Next button
+    pagination.innerHTML += '<li onclick="changePage(-2)">»</li>';
+
+    // Highlight current page number
+    [...pagination.querySelectorAll('li')].forEach(li => {
+        li.classList.remove('current');
+    });
+    pagination.querySelectorAll('li')[curtPage - startPage + 1].classList.add('current');
+}
+
+function changePage(page) {
+    let totalPages = Math.ceil(rows.length / rowsPerPage);
+
+    // Calculate new page number
+    if (page == -1) { // Previous button clicked
+        if (curtPage > 1) curtPage--;
+    } else if (page == -2) { // Next button clicked
+        if (curtPage < totalPages) curtPage++;
+    } else {
+        curtPage = page;
+    }
+
+    displayRows(rows, rowsPerPage, curtPage);
+}
+
+function handleSearch() {
+    const searchInput = document.getElementById('searchInput');
+    searchTerm = searchInput.value.toLowerCase();
+
+    // Reset pagination to the first page when searching
+    curtPage = 1;
+
+    displayRows(rows, rowsPerPage, curtPage);
+}
