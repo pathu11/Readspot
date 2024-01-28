b1d1ee95792474636616880b0a97471019f393a9
Kaumadi Pahalage <kaumadi2k2@gmail.com>
2024-01-28 11:38:53 +0530

    change database name

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_NAME','read_spot');
+define('DB_NAME','readspots');
 
 
 define('APPROOT', dirname(dirname(__FILE__)));
