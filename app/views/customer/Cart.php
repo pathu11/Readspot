cc9877b2724f2bb6e3f1fe7d90ad83d25af8e2f4
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-02-09 20:22:23 +0530

    Change cart,order tables

--- app/views/customer/Cart.php
+++ app/views/customer/Cart.php
@@ -11,63 +11,73 @@
                 <h2>My Cart</h2>
             </div>
             <div class="mycart">
-                <form action="#.php" class="cart-search">
-                    <input type="text" placeholder="Search.." name="search">
-                    <button type="submit"><img src="<?php echo URLROOT; ?>/assets/images/customer/search.png"></button> <!--path changed-->
-                </form>
+                <div class="cart-search" id="searchForm" onsubmit="handleSearch()">
+                    <input type="text" placeholder="Search.." name="search" id="searchInput">
+                </div>
                 <br>
                 <br>
                 
-                <table border="1" class="tb-cart1">
-                    <tr>
-                        <th>Book Name</th>
-                        <th>Price per book</th>
-                        <th>Quantity</th>
-                        <th>Total Price</th>
-                        <th>Checkout / Remove</th>
-                    </tr>
-                <?php foreach($data['cartDetails'] as $cart): ?>
-                    
-                    <tr>
-                        <td><?php echo $cart->book_name; ?></td>
-                        <td><?php echo $cart->price; ?></td>
-                        <td><?php echo $cart->quantity; ?></td>
-                        <td><?php echo $cart->cart_id; ?></td>
-                        <td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/purchase/<?php echo $cart->book_id; ?>" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td>
-                    </tr>
-                    <?php endforeach; ?>
-                    
+                <table border="1" class="tb-cart1" id="eventTable">
+                    <thead>
+                        <tr>
+                            <th>Book Name</th>
+                            <th>Price per book</th>
+                            <th>Quantity</th>
+                            <th>Total Price</th>
+                            <th>Checkout / Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($data['cartDetails'] as $cart): ?>
+                        <tr>
+                            <td><?php echo $cart->book_name; ?></td>
+                            <td><?php echo $cart->price; ?></td>
+                            <td><?php echo $cart->quantity; ?></td>
+                            <td><?php echo $cart->price*$cart->quantity; ?></td>
+                            <!-- <td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/purchase/<?php echo $cart->book_id; ?>" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td> -->
+                            <td class="action-buttons">
+                                <a href="<?php echo URLROOT; ?>/customer/purchase/<?php echo $cart->book_id; ?>" style="text-decoration: none;">
+                                    <button class="view-button">
+                                            <i class="fas fa-shopping-cart"></i>
+                                    </button>
+                                </a>
+                                <button class="delete-button" onclick="deleteEvent(1)">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
                 </table>
-                <table border="1" class="tb-cart2">
-                <tr><td rowspan="4">Lorem ipsum dolor sit amet</td>
-                    <td>rs200.00 (per book)</td></tr>
-                    <tr><td>3 (quantity)</td></tr>
-                    <tr><td>rs600.00 (total)</td></tr>
-                    <tr><td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/checkoutform" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td></tr>
 
-                    <tr><td rowspan="4">Lorem ipsum dolor sit amet</td>
-                    <td>rs200.00 (per book)</td></tr>
-                    <tr><td>3 (quantity)</td></tr>
-                    <tr><td>rs600.00 (total)</td></tr>
-                    <tr><td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/checkoutform" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td></tr>
 
-                    <tr><td rowspan="4">Lorem ipsum dolor sit amet</td>
-                    <td>rs200.00 (per book)</td></tr>
-                    <tr><td>3 (quantity)</td></tr>
-                    <tr><td>rs600.00 (total)</td></tr>
-                    <tr><td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/checkoutform" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td></tr>
-
-                    <tr><td rowspan="4">Lorem ipsum dolor sit amet</td>
-                    <td>rs200.00 (per book)</td></tr>
-                    <tr><td>3 (quantity)</td></tr>
-                    <tr><td>rs600.00 (total)</td></tr>
-                    <tr><td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/checkoutform" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td></tr>
-
-                    <tr><td rowspan="4">Lorem ipsum dolor sit amet</td>
-                    <td>rs200.00 (per book)</td></tr>
-                    <tr><td>3 (quantity)</td></tr>
-                    <tr><td>rs600.00 (total)</td></tr>
-                    <tr><td><div class="cart-vd"><a href="<?php echo URLROOT; ?>/customer/checkoutform" class="cart-view">Checkout</a><a href="#" class="cart-delete">Remove</a></div></td></tr>
+                <table border="1" class="tb-cart2" id="eventTable">
+                    <tbody>
+                        <?php foreach($data['cartDetails'] as $cart): ?>
+                        <tr>
+                            <td rowspan="4"><?php echo $cart->book_name; ?></td>
+                            <td><?php echo $cart->price; ?> (per book)</td>
+                        </tr>
+                        <tr>
+                            <td><?php echo $cart->quantity; ?> (quantity)</td>
+                        </tr>
+                        <tr>
+                            <td><?php echo $cart->price*$cart->quantity; ?> (total)</td>
+                        </tr>
+                        <tr>
+                            <td class="action-buttons">
+                                <a href="<?php echo URLROOT; ?>/customer/purchase/<?php echo $cart->book_id; ?>" style="text-decoration: none;">
+                                    <button class="view-button">
+                                            <i class="fas fa-shopping-cart"></i>
+                                    </button>
+                                </a>
+                                <button class="delete-button" onclick="deleteEvent(1)">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
                 </table>
             </div>
         </div>
--- app/views/customer/Order.php
+++ app/views/customer/Order.php
@@ -1,5 +1,5 @@
 <?php
-    $title = "Content";
+    $title = "My Orders";
     require APPROOT . '/views/customer/header.php'; //path changed
 ?>
     <?php
@@ -11,30 +11,39 @@
                 <h2>My Orders</h2>
             </div>
             <div class="myorder">
-            <form action="#.php" class="order-search">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit"><img src="<?php echo URLROOT; ?>/assets/images/customer/search.png"></button> <!--path changed-->
-            </form>
+            <div class="order-search" id="searchForm" onsubmit="handleSearch()">
+                <input type="text" placeholder="Search.." name="search" id="searchInput">
+            </div>
             <?php
                 require APPROOT . '/views/customer/oderdetails.php';
             ?>
             <br>
             <br>
-            <table border="1">
-                <tr>
-                    <th>Reference No.</th>
-                    <th >Delivery Status</th>
-                    <th>VIew Details</th>
-                </tr>
-                
-                <?php foreach($data['orderDetails'] as $orders): ?>
-                <tr>
-                    <td><?php echo $orders->tracking_no; ?></td>
-                    <td><?php echo $orders->status; ?></td>
-                    <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button>
-                </tr>
-                <?php endforeach; ?>
-                
+            <table border="1" id="eventTable">
+                <thead>
+                    <tr>
+                        <th>Reference No.</th>
+                        <th >Delivery Status</th>
+                        <th>VIew Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($data['orderDetails'] as $orders): ?>
+                    <tr>
+                        <td><?php echo $orders->tracking_no; ?></td>
+                        <td><?php echo $orders->status; ?></td>
+                        <!-- <td><div class="o-vd"><button type="submit" class="o-view" onclick="toggleOrder('order-details')">View</button> -->
+                        <td class="action-buttons">
+                            <button class="view-button" onclick="viewEvent(1)">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                            <button class="delete-button" onclick="deleteEvent(1)">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
             </table>
             <!-- <br>
             <br> -->
