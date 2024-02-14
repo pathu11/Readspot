be8a1e99df8a37961d4ced5520af13be2f12b347
RamathPerera <118724044+RamathPerera@users.noreply.github.com>
2024-02-14 15:26:32 +0530

    change database

--- app/config/config.php
+++ app/config/config.php
@@ -5,6 +5,10 @@ define('DB_USER','root');
 define('DB_PASS','');
 define('DB_NAME','readspot02');
 
+// define('DB_HOST','readspot.mysql.database.azure.com');
+// define('DB_USER','readspot');
+// define('DB_PASS','Admin@1234');
+// define('DB_NAME','readspot');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 // url root-public folder
