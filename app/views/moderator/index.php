e44cfd7feaa925e25b6ffa932fddd968efd37e62
Kaumadi Pahalage <136589603+Kaumadi2k@users.noreply.github.com>
2024-02-17 19:48:14 +0530

    start moderator dashboard

--- app/views/moderator/index.php
+++ app/views/moderator/index.php
@@ -7,11 +7,34 @@
   <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/admin/nav.css" />
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/assets/css/moderator/style.css" />
   <title>Moderator Dashboard</title>
 </head>
 <body>
   <?php require APPROOT . '/views/moderator/nav.php';?>
 
+  <div class="dashboard">
+    <div class="stat-itmes">
+      <div class="grid">
+        <div class="grid-item">
+          <span>Ongoing Challenges</span>
+        </div>
+        <div class="grid-item">
+          <span>Ongoing Challenges</span>
+        </div>
+        <div class="grid-item">
+          <span>Ongoing Challenges</span>
+        </div>
+      </div>
+      <div class="chart">
+
+      </div>
+      
+    </div>
+    <div class="messages">
+      
+    </div>
+  </div>
 
 </body>
 </html>
