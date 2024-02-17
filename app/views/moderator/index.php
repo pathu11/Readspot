12e9b967c1004fb93e753717b72f34453f7d7c05
Kaumadi Pahalage <136589603+Kaumadi2k@users.noreply.github.com>
2024-02-17 21:26:33 +0530

    update moderator dashboard

--- app/views/moderator/index.php
+++ app/views/moderator/index.php
@@ -17,13 +17,16 @@
     <div class="stat-itmes">
       <div class="grid">
         <div class="grid-item">
+          <i class="fa fa-solid fa-chess" style="color: #B197FC;"></i>
           <span>Ongoing Challenges</span>
         </div>
         <div class="grid-item">
-          <span>Ongoing Challenges</span>
+          <i class="fa fa-solid fa-newspaper" style="color: #FFD43B;"></i>
+          <span>Top Contents</span>
         </div>
         <div class="grid-item">
-          <span>Ongoing Challenges</span>
+          <i class="fa fa-solid fa-calendar-day" style="color: #74C0FC;"></i>
+          <span>Pending Events</span>
         </div>
       </div>
       <div class="chart">
@@ -31,8 +34,21 @@
       </div>
       
     </div>
-    <div class="messages">
-      
+    <div class="message-panel">
+      <h3>Messages for you</h3>
+      <div class="messages">
+        <div class="message">
+          <i class="bx bxs-user-circle icon"></i>
+          <span>Admin</span>
+          <p>There are some events you have to approve quickly</p>
+        </div>
+
+        <div class="message">
+          <i class="bx bxs-user-circle icon"></i>
+          <span>Admin</span>
+          <p>There are some events you have to approve quickly</p>
+        </div>
+      </div>
     </div>
   </div>
 
