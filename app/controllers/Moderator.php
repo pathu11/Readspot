2101204e0c864a85b0ce11d92dfeb64a8be209ca
Kaumadi Pahalage <136589603+Kaumadi2k@users.noreply.github.com>
2024-02-21 17:07:51 +0530

    complete moderator quiz creation

--- app/controllers/Moderator.php
+++ app/controllers/Moderator.php
@@ -129,8 +129,101 @@
     }
 
     public function createChallengeQuestions(){
-      $this->view('moderator/createChallengeQuestions');
+      if (!isLoggedIn()) {
+          redirect('landing/login');
+      } else {
+          $user_id = $_SESSION['user_id'];
+          $moderatorDetails = $this->moderatorModel->findmoderatorById($user_id);
+          $quiz_id = $this->moderatorModel->getQuizID();       
+  
+          if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+              $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+  
+              $all_questions_added = true;
+  
+              for ($i = 1; $i <= 5; $i++) {
+                  $question = $_POST["q$i"];
+                  $option1 = $_POST["q$i-opt1"];
+                  $option2 = $_POST["q$i-opt2"];
+                  $option3 = $_POST["q$i-opt3"];
+                  $correctAnswer = $_POST["q$i-answer"];
+  
+                  $data = [
+                      'moderatorDetails' => $moderatorDetails,
+                      'moderatorName' => $moderatorDetails[0]->name,
+                      'question' => $question,
+                      'option1' => $option1,
+                      'option2' => $option2,
+                      'option3' => $option3,
+                      'correctAnswer' => $correctAnswer,
+                      'quiz_id' => $quiz_id,
+                      'question_id'=>$i,
+  
+                      'question_err' => '',
+                      'option1_err' => '',
+                      'option2_err' => '',
+                      'option3_err' => '',
+                      'correctAnswer_err' => '',
+                  ];
+  
+                  if (empty($data['question'])) {
+                      $data['question_err'] = 'Please enter the question';      
+                      $all_questions_added = false;
+                  }
+                  if (empty($data['option1'])) {
+                      $data['option1_err'] = 'Please enter the option 1';      
+                      $all_questions_added = false;
+                  }
+                  if (empty($data['option2'])) {
+                      $data['option2_err'] = 'Please enter the option 2';      
+                      $all_questions_added = false;
+                  }
+                  if (empty($data['option3'])) {
+                      $data['option3_err'] = 'Please enter the option 3';      
+                      $all_questions_added = false;
+                  }
+                  if (empty($data['correctAnswer'])) {
+                      $data['correctAnswer_err'] = 'Please enter the correct answer';      
+                      $all_questions_added = false;
+                  }
+  
+                  if ($all_questions_added && $this->moderatorModel->addQuestion($data)) {
+                      // All questions added successfully
+                      flash('add_success', 'You have added the questions successfully');
+                  }
+              }
+              redirect('moderator/createChallengeQuestions');
+          } else {
+              for ($i = 1; $i <= 5; $i++) {
+                  $question = '';
+                  $option1 = '';
+                  $option2 = '';
+                  $option3 = '';
+                  $correctAnswer = '';
+  
+                  $data = [
+                      'moderatorDetails' => $moderatorDetails,
+                      'moderatorName' => $moderatorDetails[0]->name,
+                      'question' => $question,
+                      'option1' => $option1,
+                      'option2' => $option2,
+                      'option3' => $option3,
+                      'correctAnswer' => $correctAnswer,
+                      'quiz_id' => $quiz_id,
+  
+                      'question_err' => '',
+                      'option1_err' => '',
+                      'option2_err' => '',
+                      'option3_err' => '',
+                      'correctAnswer_err' => '',
+                  ];
+  
+                  $this->view('moderator/createChallengeQuestions', $data);
+              } 
+          }
+      }
     }
+  
 
     public function events(){
       if (!isLoggedIn()) {
